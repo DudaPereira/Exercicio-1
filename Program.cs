a6967e3f8f607e564a8a680d20eaba3d5cf3badf
DEV <dev@info.local>
2020-11-13 16:38:55 -0300

    Exercício 1 finalizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("Bem vindo ao site que converte sua idade em: Meses, Dias, Horas e Minutos");
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("Qual é a sua idade?");
+            int resposta = int.Parse(Console.ReadLine());
+
+            int meses = resposta * 12;
+            int dias = resposta * 365;
+            double horas = resposta * 8.760;
+            double minutos = resposta * 525.600;
+
+            Console.WriteLine("Resposta:" + meses + "meses,"  + dias + "dias," + horas + "horas" + minutos + "minutos,");
+        }
+    }
+}
